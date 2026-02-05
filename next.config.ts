29f4f349a9fa8492659d77193776ae152bedd991
Walie-Matthieu <whoumad@gmail.com>
2026-02-05 23:51:49 +0100

    modif basePath

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/snake-game",
+  assetPrefix: "/snake-game/",
   reactStrictMode: false,
   // No custom webpack rules here to avoid the "Webpack is configured while Turbopack is not" warning.
 };
